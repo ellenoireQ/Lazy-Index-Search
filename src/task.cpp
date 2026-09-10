862cd3716a91c07323703242eb5345517458072e
Fitrian Musya <166298239+ellenoireQ@users.noreply.github.com>
2026-09-10 02:09:40 +0000

    feat(task): implement get_status method for task retrieval

--- src/task.cpp
+++ src/task.cpp
@@ -6,6 +6,16 @@ void Task::mark(std::string path, TaskQueue tsk)
     this->task[path] = tsk;
 }
 
+TaskQueue Task::get_status(std::string path)
+{
+    std::lock_guard<std::mutex> guard(this->mutex_lock);
+    if (this->task.find(path) == this->task.end())
+    {
+        this->task[path] = TaskQueue::Pending;
+    }
+    return this->task[path];
+}
+
 void Task::spawn()
 {
 }
