035ab2e484d488ebaa6470fbf84962f8a9184f86
ellenoireQ <ketchupnaiso@gmail.com>
2026-09-10 09:30:06 +0700

    refactor(engine): remove unused find functionality

--- src/includes/find.hpp
+++
@@ -1,9 +0,0 @@
-#include <filesystem>
-#include <includes/config.hpp>
-
-namespace fclasses
-{
-    namespace fs = std::filesystem;
-    /** finding file based by name */
-    void find(const char *tgetf, Config &config);
-};
